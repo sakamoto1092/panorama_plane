37adb4d6bf9563ea7bd5a1861d209fd0e1e62a97
avon_orz <sakamoto1092@yahoo.co.jp>
2013-09-25 04:18:52 +0900

    a

--- Release/src/mysurf_panorama.d
+++
@@ -1,6 +0,0 @@
-src/mysurf_panorama.d src/mysurf_panorama.o: ../src/mysurf_panorama.cpp \
- /usr/local/include/opencv/cv.h /usr/local/include/opencv/highgui.h
-
-/usr/local/include/opencv/cv.h:
-
-/usr/local/include/opencv/highgui.h:
